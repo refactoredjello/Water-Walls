470e15c5c5d70b62b1e4ad809061ff8fb2d70d2c
refactoredjello <gil.mandler@gmail.com>
2018-02-04 13:28:38 -0500

    Calculation is moved to separate function

--- waterWalls.js
+++ waterWalls.js
@@ -1,31 +1,40 @@
 const findGreatestWaterWalls = elevationMap => {
-  let start, end, tracked = [], previousTracked = [], calculated = false, waterWalls = []
+  let start, prevStart, end, tracked = [], previousTracked = [], calculated = false, waterWalls = []
+  
+  const makeWaterWall = (i, addPreviousFlag) => {
+    end = i 
+    if (addPreviousFlag) {
+      
+      start = prevStart
+      tracked = tracked.concat(previousTracked)
+      
+    }
+    lower = elevationMap[start] < elevationMap[end] ? elevationMap[start] : elevationMap[end]
+
+    let water = tracked.reduce((acc, val) => acc += lower - val, 0)
+
+    waterWalls.push([start + 1, end + 1, water])
+    calculated = true
+    previousTracked = [...tracked]
+    prevStart = start
+    tracked = []
+  }
 
   for (let i = 0; i < elevationMap.length; i++) {
- 
+    let current = elevationMap[i]
+
     if (calculated || i === 0) {
 
       start = i
       calculated = false;
 
-    } else if ((elevationMap[i] >= elevationMap[start] || !elevationMap[i + 1]) && tracked.length > 0 ) {
-
-      end = i 
-      lower = elevationMap[start] < elevationMap[end] ? elevationMap[start] : elevationMap[end]
-
-      let water = tracked.reduce((acc, val) => {
-        return acc += lower - val
-      }, 0)
-  
-      waterWalls.push([start + 1, end + 1, water])
+    } else if ((current >= elevationMap[start] || !elevationMap[i + 1]) && tracked.length > 0 ) {
       
-      calculated = true
-      previousTracked = [...tracked]
-      tracked = []
-      i--
+      let addPreviousFlag = elevationMap[prevStart] > current 
+      makeWaterWall(i--, addPreviousFlag)
 
-    } else if (elevationMap[i] < elevationMap[start]) {
-      tracked.push(elevationMap[i])
+    } else if (current < elevationMap[start]) {
+      tracked.push(current)
 
     }
   }
