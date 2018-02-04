a844ef99d3f6d3a152f5d8c3d81b4726bc5967e4
refactoredjello <gil.mandler@gmail.com>
2018-02-03 21:13:13 -0500

    Adds test for non-ascending water walls height

--- waterWalls.test.js
+++ waterWalls.test.js
@@ -9,3 +9,8 @@ test('finds the largest water walls', () => {
   let expected = findGreatestWaterWalls([5, 3, 7, 2, 6, 4, 5, 9, 1, 2])
   expect(expected).toEqual([3,8,11]);
 });
+
+test('finds the largest water walls if walls are not in acsending height', () => {
+  let expected = findGreatestWaterWalls([2, 1, 9, 5, 4, 6, 2, 7, 3, 5])
+  expect(expected).toEqual([3,8,11]);
+});
